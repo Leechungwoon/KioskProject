1444d50d000545909cfa33c8f5bd4f105929b822
Leechungwoon <dlcjddns22@gmail.com>
2025-10-23 11:14:04 +0900

    Lv2 main (MenuItem 객체 생성)

--- src/kiosk/Main.java
+++ src/kiosk/Main.java
@@ -1,6 +1,8 @@
 package kiosk;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -12,58 +14,56 @@ public class Main {
         //인터페이스: 콘솔
         //예외처리: 입력값으로 허용한 값 이외의 값이 입력되었을 때와 같은 오류 처리 고민
 
+        //설명: 메뉴아이템이 본인을 가질 수 없다. -> 호출할 수 없다
+        //메인에서 메뉴아이템을 호출해야하기에 list는 메인에 적고 접근제어자를 가질 수 없다.
+        List<MenuItem> menuItem = new ArrayList<>();
+
         //1. Scanner 입력 생성
         Scanner sc = new Scanner(System.in);
 
-        //속성
-        //2.  햄버거 메뉴판
-        System.out.println("[ SHAKESHACK MENU ]");
-        System.out.println("1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거\n" +
-                "2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거\n" +
-                "3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거\n" +
-                "4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거\n" +
-                "0. 종료      | 종료");
+        //객체 생성 (인스턴스화)
+        MenuItem menuItem1 = new MenuItem("ShackBurger", "W 6.9", "토마토, 양상추, 쉑소스가 토핑된 치즈버거");
+        MenuItem menuItem2 = new MenuItem("SmokeBurger", "W 8.9", "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
+        MenuItem menuItem3 = new MenuItem("cheeseBurger", "W 6.9", "포테이토 번과 비프패티, 치즈 토핑된 치즈버거");
+        MenuItem menuItem4 = new MenuItem("HamBurger", "W 5.4", "비프패티를 기반으로 야패가 들어간 기본버거");
 
-        //3. Burger 생성
-        String burger1 = "ShackBuger";
-        String burger2 = "SmokeBuger";
-        String burger3 = "CheeseBuger";
-        String burger4 = "HamBuger";
+        menuItem.add(menuItem1);
+        menuItem.add(menuItem2);
+        menuItem.add(menuItem3);
+        menuItem.add(menuItem4);
 
-        //4. 가격 설정
-        String bPrice1 = "W 6.9";
-        String bPrice2 = "W 8.9";
-        String bPrice3 = "W 6.9";
-        String bPrice4 = "W 5.4";
+            //2.  햄버거 메뉴판
+            System.out.println("[ SHAKESHACK MENU ]");
+            System.out.println("1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거\n" +
+                    "2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거\n" +
+                    "3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거\n" +
+                    "4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거\n" +
+                    "0. 종료      | 종료");
 
-        //5. 메뉴 설명
-        String bIntro1 = "토마토, 양상추, 쉑소스가 토핑된 치즈버거";
-        String bIntro2 = "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거";
-        String bIntro3 = "포테이토 번과 비프패티, 치즈 토핑된 치즈버거";
-        String bIntro4 = "비프패티를 기반으로 야패가 들어간 기본버거";
+            // 3. 출력 메세지
+            System.out.println("주문할 햄버거 숫자를 입력하세요: ");
+            int foodOrder = sc.nextInt();
 
-        // 6. 출력 메세지
-        System.out.println("주문할 햄버거 숫자를 입력하세요: ");
-        int foodOrder = sc.nextInt();
-
-        // 7. 음식 선택 및 주문하기
-        switch (foodOrder){
-            case 1 :
-                System.out.println("1." + burger1 + "|" + bPrice1 + "|" + bIntro1);
-                break;
-            case 2 :
-                System.out.println("2." + burger2 + "|" + bPrice2 + "|" + bIntro2);
-            case 3 :
-                System.out.println("3." + burger3 + "|" + bPrice3 + "|" + bIntro3);
-            case 4 :
-                System.out.println("4." + burger4 + "|" + bPrice4 + "|" + bIntro4);
-            case 0 :
-                System.out.println("0. 종료 | 종료");
-        }
-
-        //8. 주문 완료 문구
-        System.out.println("주문이 완료됐습니다.");
-
+            // 4. 음식 선택 및 주문하기
+            switch (foodOrder) {
+                case 1:
+                    System.out.println("1." + menuItem1);
+                    break;
+                case 2:
+                    System.out.println("2." + menuItem2);
+                    break;
+                case 3:
+                    System.out.println("3." + menuItem3);
+                    break;
+                case 4:
+                    System.out.println("4." + menuItem4);
+                    break;
+                case 0:
+                    System.out.println("0. 종료 | 종료");
+                    break;
+                default:
+                    System.out.println("주문이 취소됐습니다.");
+            }
 
     }
 }
