494d187771a7109cad4640ef1570e7951f39745d
Leechungwoon <dlcjddns22@gmail.com>
2025-10-29 15:27:54 +0900

    List 버거리스트 변경 및 음료, 디저트 리스트 추가

--- src/kiosk/Main.java
+++ src/kiosk/Main.java
@@ -1,7 +1,7 @@
 package kiosk;
 
-import java.awt.*;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,58 +12,40 @@ public class Main {
         //인터페이스: 콘솔
         //예외처리: 입력값으로 허용한 값 이외의 값이 입력되었을 때와 같은 오류 처리 고민
 
-        //1. Scanner 입력 생성
-        Scanner sc = new Scanner(System.in);
+        //Menu 객체 생성을 통해 이름 설정
+        //Menu 클래스 내 있는 List<MenuItem> 에 MenuItem 객체 생성하면서 삽입
+        List<MenuItem> burgerList = new ArrayList<>();
+        List<MenuItem> drinkList = new ArrayList<>();
+        List<MenuItem> desertList = new ArrayList<>();
 
-        //속성
-        //2.  햄버거 메뉴판
-        System.out.println("[ SHAKESHACK MENU ]");
-        System.out.println("1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거\n" +
-                "2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거\n" +
-                "3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거\n" +
-                "4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거\n" +
-                "0. 종료      | 종료");
+        //객체 생성 (인스턴스화)
+        Menu burgerMenu = new Menu("Burgers", burgerList);
+        burgerList.add(new MenuItem("ShackBurger", 6900, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        burgerList.add(new MenuItem("SmokeBurger", 8900, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        burgerList.add(new MenuItem("cheeseBurger", 6900, "포테이토 번과 비프패티, 치즈 토핑된 치즈버거"));
+        burgerList.add(new MenuItem("HamBurger", 5400, "비프패티를 기반으로 야패가 들어간 기본버거"));
 
-        //3. Burger 생성
-        String burger1 = "ShackBuger";
-        String burger2 = "SmokeBuger";
-        String burger3 = "CheeseBuger";
-        String burger4 = "HamBuger";
+        Menu drinkMenu = new Menu("Drinks", drinkList);
+        drinkList.add(new MenuItem("Protein", 4000, "단백질 함량이 30g 들어간 초코맛 프로틴"));
+        drinkList.add(new MenuItem("Milk", 2000, "고소한 맛의 흰 우유"));
+        drinkList.add(new MenuItem("CokeZero", 2000, "제로 칼로리이며, 부담 없는 햄버거와 잘 어울리는 최고의 조합"));
+        drinkList.add(new MenuItem("Coke", 2000, "오리지널 콜라를 즐길 수 있는 환상의 조합"));
 
-        //4. 가격 설정
-        String bPrice1 = "W 6.9";
-        String bPrice2 = "W 8.9";
-        String bPrice3 = "W 6.9";
-        String bPrice4 = "W 5.4";
+        Menu desertsrMenu = new Menu("Deserts", desertList);
+        desertList.add(new MenuItem("Protein bread", 3000, "다이어터에게 적합한 빵"));
+        desertList.add(new MenuItem("Nonegg Bread", 2000, "계란 알러지 있어도 먹을 수 있는 빵"));
+        desertList.add(new MenuItem("Protein Cookie", 2000, "제로 콜라와 환상인 제로 당 쿠키"));
+        desertList.add(new MenuItem("Original Cookie", 4000, "전통이 함께하는 오리지널 쿠키"));
 
-        //5. 메뉴 설명
-        String bIntro1 = "토마토, 양상추, 쉑소스가 토핑된 치즈버거";
-        String bIntro2 = "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거";
-        String bIntro3 = "포테이토 번과 비프패티, 치즈 토핑된 치즈버거";
-        String bIntro4 = "비프패티를 기반으로 야패가 들어간 기본버거";
+        // List에 카테고리 추가하기
+        List<Menu> menus = new ArrayList<>();
+        menus.add(burgerMenu);
+        menus.add(drinkMenu);
+        menus.add(desertsrMenu);
 
-        // 6. 출력 메세지
-        System.out.println("주문할 햄버거 숫자를 입력하세요: ");
-        int foodOrder = sc.nextInt();
-
-        // 7. 음식 선택 및 주문하기
-        switch (foodOrder){
-            case 1 :
-                System.out.println("1." + burger1 + "|" + bPrice1 + "|" + bIntro1);
-                break;
-            case 2 :
-                System.out.println("2." + burger2 + "|" + bPrice2 + "|" + bIntro2);
-            case 3 :
-                System.out.println("3." + burger3 + "|" + bPrice3 + "|" + bIntro3);
-            case 4 :
-                System.out.println("4." + burger4 + "|" + bPrice4 + "|" + bIntro4);
-            case 0 :
-                System.out.println("0. 종료 | 종료");
-        }
-
-        //8. 주문 완료 문구
-        System.out.println("주문이 완료됐습니다.");
-
-
+        // Kiosk 객체 생성
+        Kiosk kiosk = new Kiosk(menus); // 태현 튜터님과 한 부분
+        // Kiosk 객체 생성
+        kiosk.start(); // 태현 튜터님과 한 부분
     }
 }
